3a0a3c4cbd25d8b5753c20c82064d22d205aab6a
null-a0 <23f1001572@ds.study.iitm.ac.in>
2026-05-17 23:57:19 +0530

    Fix alembic to use async engine directly

--- backend/alembic/env.py
+++ backend/alembic/env.py
@@ -66,14 +66,10 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_async_migrations() -> None:
-    """In this scenario we need to create an Engine
-    and associate a connection with the context.
+    from sqlalchemy.ext.asyncio import create_async_engine
 
-    """
-
-    connectable = async_engine_from_config(
-        config.get_section(config.config_ini_section, {}),
-        prefix="sqlalchemy.",
+    connectable = create_async_engine(
+        settings.DATABASE_URL,
         poolclass=pool.NullPool,
     )
 
